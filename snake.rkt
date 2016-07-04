20c5e211bf3cf042377cb3e0523d92e4188ea098
Stephen Grice <stephengrice@live.com>
2016-07-04 13:28:11 -0400

    Fix problem with (random) call

--- snake.rkt
+++ snake.rkt
@@ -26,7 +26,7 @@
 	(send dc draw-rectangle (* x square-size) (* y square-size) square-size square-size))
 	; random-square: gets random square in grid
 (define (random-square)
-	(cons (random 1 (car (grid-max)) (current-pseudo-random-generator)) (random 1 (cdr (grid-max)) (current-pseudo-random-generator))))
+	(cons (random (+ 1 (car (grid-max)))) (random (cdr (grid-max)))))
 	; grid-max: returns a pair containing the max x and y values that are onscreen in grid system
 (define (grid-max)
 	(cons (floor (/ (car window-size) square-size)) (floor (/ (cdr window-size) square-size))))
